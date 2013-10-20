dc6a35410e89f43a9ace60134f86bec706a4a199
hdugan <hilarydugan@gmail.com>
2013-10-20 12:00:23 -0500

    added reference

--- R/k.crusius.R
+++ R/k.crusius.R
@@ -1,1 +1,8 @@
- 
+# ---Author: Hilary Dugan, 2013-10-20 --- 
+
+
+
+# -- References 
+#CRUSIUS, JOHN, AND RIK WANNINKHOF. 2003
+#Gas transfer velocities measured at low wind speed over a lak.
+#Limnology and Oceanography. 48(3): 1010:1017.
