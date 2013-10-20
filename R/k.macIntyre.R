f7032d08e94474c6cc9181bd03612cbcedcc0a17
hdugan <hilarydugan@gmail.com>
2013-10-20 11:19:45 -0500

    added reference

--- R/k.macIntyre.R
+++ R/k.macIntyre.R
@@ -1,1 +1,6 @@
  
+
+# -- References 
+#MACINTRYE, sALLY, ANDERS JONSSON, MATS JANSSON, JAN ABERG, DAMON E. TURNEY AND SCOTT D. MILLER.
+#2010. Buoyancy flux, turbulence, and the gas transfer coefficient in a stratified lake.
+#Geophysical Research Letters. 37: L24604. doi:10.1029/2010GL044164 
