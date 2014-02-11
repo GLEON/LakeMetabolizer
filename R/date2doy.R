b06671610e84accc10e419b5cfcd467499c1ae8b
Ryan Batt <battrd@gmail.com>
2014-02-11 14:04:37 -0600

    convert POSIX vector into DoY
    DoY year being julian day + fraction of day. Requires posix format,
    which can be established during data import. For example:
     read.table(“trout bog.wnd”, sep="\t", header=TRUE,
    colClasses=c("POSIXct","numeric"))

---
+++ R/date2doy.R
@@ -0,0 +1,8 @@
+date.2doy <- function(x){
+	stopifnot(any(grepl("^POSIX",class(wo[,1]))))
+	day <- as.numeric(format(x, "%j"))
+	pat <- quote("([0-9]{2}:){2}[0-9]{2}")
+	midnight <- gsub(pat, "00:00:00", x)
+	frac <- as.numeric(difftime(x, midnight, units="days"))
+	day+frac
+}
