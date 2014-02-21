0fce51fb5db51119bd99b85cc0ca750949a278cc
Luke Winslow <lawinslow@gmail.com>
2014-02-21 17:03:23 -0500

    First steps of metab.bayesian cleaup.
    Also added metab.bayes documentation file

--- R/metab.bayesian.R
+++ R/metab.bayesian.R
@@ -13,38 +13,6 @@ source("date2doy.R")
 source("o2.saturation.R")
 source("longestRun.R")
 
-# ====================
-# = Read in Raw Data =
-# ====================
-# wind data >> scale.exp.wind() >> k.cole() >> k600.2.kGAS() >>
-wo <- read.table("../inst/extdata/troutbog.wnd", sep="\t", header=TRUE, colClasses=c("POSIXct","numeric"))
-po <- read.table("../inst/extdata/troutbog.par", sep="\t", header=TRUE, colClasses=c("POSIXct","numeric"))
-to <- read.table("../inst/extdata/troutbog.wtr", sep="\t", header=TRUE, colClasses=c("POSIXct",rep("numeric",10)))[,1:2]
-doo <- read.table("../inst/extdata/troutbog.doobs", sep="\t", header=TRUE, colClasses=c("POSIXct","numeric"))
-
-# ===========================
-# = Combine & Organize Data =
-# ===========================
-#merge
-d1 <- merge(to, doo, all=TRUE)
-d2 <- merge(wo, po, all=TRUE)
-d3 <- merge(d1,d2, all=TRUE)
-
-#convert to DoY format (not actually needed in this case, but conventient to have)
-d3[,1] <- date2doy(d3[,1])
-
-#subset to the portion of the data set with the most consecutive observations (not necessary)
-data0 <- d3[longestRun(d3),]
-names(data0) <- c("DoY", "Temp", "DO", "Wind", "PAR") # rename columns while still data frame
-data0 <- as.matrix(data0) # convert to matrix
-row.names(data0) <- NULL # remove row names left over from d3
-data0 <- data0[data0[,"DoY"]>=319&data0[,"DoY"]<320,] # subset for fast trial run
-
-Freq <- median(diff(data0[,"DoY"])) # determine the sampling frequency; i have a function for mode if we are worried about it
-
-wind <- scale.exp.wind(data0[,"Wind"], 2) # convert wind
-Kvec <- k600.2.kGAS(k.cole(wind)*Freq, data0[,"Temp"], "O2") # calculate K for relevant sampling frequency
-
 
 # =========================
 # = Define the Jags Model =
@@ -77,39 +45,12 @@ j2Mod <- function(){
 	sigmaV <- 1/sqrt(tauV)
 	sigmaW <- 1/sqrt(tauW)	
 }
-write.model(j2Mod, "j2Mod")
 
-# ===========================================
-# = Define objects to be used in jags model =
-# ===========================================
-#Supply elements of U (PAR, log(temp), 0's)
-U <- matrix(NA, nrow=nrow(data0), ncol=2)
-U[,1] <- data0[,"PAR"] # filler PAR Values
-U[,2] <- log(data0[,"Temp"]) # filler log(temp) values
+modfile = tempfile('j2mod')
+write.model(j2Mod, modfile)
 
-#Supply kP (for K), and cP (for C)
-# ** I am making up these values for now
-kP <- matrix(NA, nrow=length(Kvec), ncol=2)
-kP[,1] <- Kvec # means for K
-kP[,2] <- sum(Kvec)/length(Kvec)*0.1 # variances for K
-cP <- matrix(NA, nrow=2, ncol=2)
-cP[1,1] <- 0 # prior mean of GPP coefficient (C[1,1]*PAR=GPP)
-cP[1,2] <- 1E5 # prior variance of GPP coefficient
-cP[2,1] <- 0 # prior mean of R coefficient (C[2,1]*log(Temp)=R)
-cP[2,2] <- 1E5 # prior variance of R coefficient
 
-#Supply observed DO
-Y <- data0[,"DO"]
 
-#Supply saturated DO
-satO <- o2.at.sat(data0[,"Temp"], 716)
-
-#Supply Zmix
-Zmix <- rep(1, length(Kvec)) # filler values
-
-# Put in final format supplied to jags
-data <- list(Y=Y, N=length(Y), U=U, kP=kP, cP=cP, satO=satO, a0=Y[1], Zmix=Zmix)
-params <- c("C", "sigmaV", "sigmaW")
 
 
 # ================================
@@ -117,7 +58,7 @@ params <- c("C", "sigmaV", "sigmaW")
 # ================================
 bayesFit <- function(data, params, tend="median", ...){ #function that writes jags model, traces params, supplies data, etc
 
-	jags.m <- jags(data, NULL, parameters.to.save=params, "j2Mod", n.chains=3, n.iter=1E4, n.burnin=5E3)
+	jags.m <- jags(data, NULL, parameters.to.save=params, modfile, n.chains=3, n.iter=1E4, n.burnin=5E3)
 	
 	tF <- function(x, tend){ # tendency function
 		switch(tend,
@@ -151,19 +92,98 @@ bayesFit <- function(data, params, tend="median", ...){ #function that writes ja
 }
 
 
-test <- bayesFit(data, params)
+
+metab.bayes = function(do.obs, do.sat, k.gas, z.mix, date.times, irr, wtr){
+  
+  
+  if(!all(c(is.numeric(do.obs), is.numeric(do.sat), is.numeric(k.gas), 
+            is.numeric(z.mix), is.numeric(irr), is.numeric(wtr)))){
+    
+    error('All inputs to metab.bayes must be numeric vectors')
+  }
+  
+  # ===========================================
+  # = Define objects to be used in jags model =
+  # ===========================================
+  #Supply elements of U (PAR, log(temp), 0's)
+  U <- matrix(NA, nrow=length(irr), ncol=2)
+  U[,1] <- irr #data0[,"PAR"] # filler PAR Values
+  U[,2] <- log(wtr) # log(data0[,"Temp"]) # filler log(temp) values
+  
+  #Supply kP (for K), and cP (for C)
+  # ** I am making up these values for now
+  kP <- matrix(NA, nrow=length(k.gas), ncol=2)
+  kP[,1] <- k.gas # means for K
+  kP[,2] <- sum(k.gas)/length(k.gas)*0.1 # variances for K
+  cP <- matrix(NA, nrow=2, ncol=2)
+  cP[1,1] <- 0 # prior mean of GPP coefficient (C[1,1]*PAR=GPP)
+  cP[1,2] <- 1E5 # prior variance of GPP coefficient
+  cP[2,1] <- 0 # prior mean of R coefficient (C[2,1]*log(Temp)=R)
+  cP[2,2] <- 1E5 # prior variance of R coefficient
+  
+  
+  # Put in final format supplied to jags
+  data <- list(Y=do.obs, N=length(Y), U=U, kP=kP, cP=cP, satO=do.sat, a0=do.obs[1], Zmix=z.mix)
+  params <- c("C", "sigmaV", "sigmaW")
+  
+  output <- bayesFit(data, params)
+  return(output[[2]])
+  
+}
+
+
+
+# ====================
+# = Read in Raw Data =
+# ====================
+# wind data >> scale.exp.wind() >> k.cole() >> k600.2.kGAS() >>
+wo <- read.table("../inst/extdata/troutbog.wnd", sep="\t", header=TRUE, colClasses=c("POSIXct","numeric"))
+po <- read.table("../inst/extdata/troutbog.par", sep="\t", header=TRUE, colClasses=c("POSIXct","numeric"))
+to <- read.table("../inst/extdata/troutbog.wtr", sep="\t", header=TRUE, colClasses=c("POSIXct",rep("numeric",10)))[,1:2]
+doo <- read.table("../inst/extdata/troutbog.doobs", sep="\t", header=TRUE, colClasses=c("POSIXct","numeric"))
+
+# ===========================
+# = Combine & Organize Data =
+# ===========================
+#merge
+d1 <- merge(to, doo, all=TRUE)
+d2 <- merge(wo, po, all=TRUE)
+d3 <- merge(d1,d2, all=TRUE)
+
+#convert to DoY format (not actually needed in this case, but conventient to have)
+d3[,1] <- date2doy(d3[,1])
+
+#subset to the portion of the data set with the most consecutive observations (not necessary)
+data0 <- d3[longestRun(d3),]
+names(data0) <- c("DoY", "Temp", "DO", "Wind", "PAR") # rename columns while still data frame
+data0 <- as.matrix(data0) # convert to matrix
+row.names(data0) <- NULL # remove row names left over from d3
+data0 <- data0[data0[,"DoY"]>=319&data0[,"DoY"]<320,] # subset for fast trial run
+
+Freq <- median(diff(data0[,"DoY"])) # determine the sampling frequency; i have a function for mode if we are worried about it
+
+wind <- scale.exp.wind(data0[,"Wind"], 2) # convert wind
+Kvec <- k600.2.kGAS(k.cole(wind)*Freq, data0[,"Temp"], "O2") # calculate K for relevant sampling frequency
+
+
+
+metab.bayes(irr=data0[,"PAR"], z.mix=rep(1, length(Kvec)), 
+            do.sat=o2.at.sat(data0[,"Temp"], 716), wtr=data0[,'Temp'],
+            k.gas=Kvec, do.obs=data0[,"DO"])
+
+
 # dev.new()
 # par(mfrow=c(3,2), mar=c(2,2,1.5,0.5), ps=10)
-nP <- ncol(test[[1]]$BUGSoutput$sims.matrix)
-parD <- c(ceiling(sqrt(nP)), floor(sqrt(nP)))
+#nP <- ncol(test[[1]]$BUGSoutput$sims.matrix)
+#parD <- c(ceiling(sqrt(nP)), floor(sqrt(nP)))
 # R2jags::traceplot(test[[1]], mfrow=c(parD))
 
-dev.new()
-par(mfrow=c(3,2), mar=c(2,2,1.5,0.5), ps=10)
-coda::traceplot(as.mcmc(test[[1]]))
+##dev.new()
+#par(mfrow=c(3,2), mar=c(2,2,1.5,0.5), ps=10)
+#coda::traceplot(as.mcmc(test[[1]]))
 
-dev.new()
-par(mfrow=c(3,2), mar=c(2,2,1.5,0.5), ps=10)
-coda::densplot(as.mcmc(test[[1]]))
+#dev.new()
+#par(mfrow=c(3,2), mar=c(2,2,1.5,0.5), ps=10)
+#coda::densplot(as.mcmc(test[[1]]))
 
 
