24e08259f0429cdd012399e2c68fd591e8073ed0
Ryan Batt <Battrd@dyn-72-33-192-77.uwnet.wisc.edu>
2014-03-18 16:27:44 -0500

    wrapper to analyze daily
    # supply a data set, specify a method by name, and it returns daily
    metabolism

---
+++ inProgress/metab.R
@@ -0,0 +1,7 @@
+# wrapper function
+# supply a data set, specify a method by name, and it returns daily metabolism
+# will update to accept further arguments to be passed to fitting functions
+
+metab <- function(data, method){
+	
+}
