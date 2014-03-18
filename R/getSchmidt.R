fe6356fa7fc1abf98d378fbbfa1617c0287ddbe0
Jordan S Read <jread@usgs.gov>
2014-03-17 19:59:01 -0500

    O2 and CO2 to 4-35° range from Raymond ea 2012

--- R/getSchmidt.R
+++ R/getSchmidt.R
@@ -6,9 +6,10 @@ getSchmidt	<-	function(temperature, gas){
 	# __Wanninkhof, Rik. "Relationship between wind speed and gas exchange over the ocean."__ 
 	# __Journal of Geophysical Research: Oceans (1978–2012) 97.C5 (1992): 7373-7382.__
 	
-	Schmidt	<-	data.frame("He"=c(377.09,19.154,0.50137,0.005669),
-		"O2"=c(1800.6,120.1,3.7818,0.047608),
-		"CO2"=c(1911.1,118.11,3.4527,0.04132),
+	Schmidt	<-	data.frame(
+		"He"=c(377.09,19.154,0.50137,0.005669),
+		"O2"=c(1568,-86.04,2.142,-0.0216),
+		"CO2"=c(1742,-91.24,2.208,-0.0219),
 		"CH4"=c(1897.8,114.28,3.2902,0.039061),
 		"SF6"=c(3255.3,217.13,6.8370,0.086070),
 		"N2O"=c(2055.6,137.11,4.3173,0.05435),
@@ -23,7 +24,7 @@ getSchmidt	<-	function(temperature, gas){
 	C	<-	unlist(Schmidt[gas])[3]
 	D	<-	unlist(Schmidt[gas])[4]
 
-	Sc = as.numeric(A-B*temperature+C*temperature^2-D*temperature^3)
+	Sc = as.numeric(A-B*temperature+C*temperature^2+D*temperature^3)
 
 	return(Sc)
 }
