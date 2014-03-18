add639f44a8df4f9c596694318ebd78b6c6eca11
Ryan Batt <Battrd@dyn-72-33-192-77.uwnet.wisc.edu>
2014-03-18 17:40:56 -0500

    small

--- inProgress/metab.R
+++ inProgress/metab.R
@@ -6,8 +6,13 @@
 # library("LakeMetabolizer")
 # source("/Users/Battrd/Documents/School&Work/WiscResearch/LakeMetabolizer/inProgress/ryanData.R")
 # library("LakeMetabolizer")
-data <- LakeMetabolizer:::ryanData()
+
 metab <- function(data, method){
 	require("plyr")
+	
+	data0 <- LakeMetabolizer:::ryanData()
+	data0[,"Year"] <- as.integer(format.Date(data0[,"date"], "%Y"))
+	data <- addNA(data0)
+	
 	
 }
