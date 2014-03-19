afb0065abdf53682190c47162b065a89dddc79a4
rbatt <battrd@gmail.com>
2014-03-19 10:03:10 -0500

    buffed addNAs

--- R/addNAs.R
+++ R/addNAs.R
@@ -6,13 +6,18 @@ addNAs <- function(x){
 			ux[which.max(tabulate(match(x, ux)))]
 	}
 	ex <- round(Mode(1/diff(x[,"doy"])))
+	mins <- 1/ex*24*60
+	is.wholenumber <- function(x, tol = .Machine$double.eps^0.5){abs(x - round(x)) < tol}
+	if(!is.wholenumber(mins)){warning("Time between samples not whole number")}
 	x1 <- byeShort(X=x, Expected=ex, ToCount="doy", TruncToCount=TRUE)
 	if(nrow(x1)==0){
 		return(x1)
 	}
-	Range <- range(x1[,"doy"]) #intentionally not truncating (having already used byeShort, I don't have to worry about starting and stopping at a specific time each day)
-	Ideal <- data.frame("RoundDoY"=round(seq(Range[1], Range[2], by=(1/ex)),rdig))
-	x1[,"RoundDoY"] <- round(x1[,"doy"], rdig)
+	Range <- range(x1[,"date"]) #intentionally not truncating (having already used byeShort, I don't have to worry about starting and stopping at a specific time each day)
+	# Ideal <- data.frame("RoundDoY"=round(seq(Range[1], Range[2], by=(1/ex)),rdig))
+	Ideal <- data.frame("date"=seq(Range[1], Range[2], by=paste(mins, "mins")))
+	# x1[,"RoundDoY"] <- round(x1[,"doy"], rdig)
+	
 	print(paste("NA's added to fill in time series:",dim(Ideal)[1]-dim(x1)[1], sep=" "))
 	flush.console()
 	x2 <- merge(x1, Ideal, all.x=TRUE, all.y=TRUE)
--- inProgress/metab.R
+++ inProgress/metab.R
@@ -2,20 +2,24 @@
 # supply a data set, specify a method by name, and it returns daily metabolism
 # will update to accept further arguments to be passed to fitting functions
 
-# detach(package:LakeMetabolizer)
-# install.packages("/Users/Battrd/Documents/School&Work/WiscResearch/LakeMetabolizer", repos=NULL, type="source")
-# library("LakeMetabolizer")
+install.packages("/Users/Battrd/Documents/School&Work/WiscResearch/LakeMetabolizer", repos=NULL, type="source")
+library("LakeMetabolizer")
 # source("/Users/Battrd/Documents/School&Work/WiscResearch/LakeMetabolizer/inProgress/ryanData.R")
 # library("LakeMetabolizer")
 
-metab <- function(data, method, fitBy=c("doy","year")){
+metab <- function(data, method){
 	require("plyr")
 	
 	data0 <- LakeMetabolizer:::ryanData()
 	
-	data <- LakeMetabolizer:::addNAs(data0)
-	# NOTE: The above removes days that have many missing ROWS; to remove days with many NA's, do:
-	# data <- addNA(data0[complete.cases(data0),])
+	# Removes days with many missing ROWS:
+	# data <- LakeMetabolizer:::addNAs(data0)
+	
+	# Removes days with many NA's:
+	data <- LakeMetabolizer:::addNAs(data0[complete.cases(data0),])
+	
+	ids <- id(list(data[,"year"],trunc(data[,"doy"]))) # ID chunks to be analyzed
+	
 	
 	
 	
