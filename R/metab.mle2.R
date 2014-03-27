7a5ebf257e8287392096b7b9a4d91d64ac6b4503
Luke Winslow <lawinslow@gmail.com>
2014-03-27 07:10:07 -0500

    Cleaning up @rBatt code.

--- R/metab.mle2.R
+++ R/metab.mle2.R
@@ -17,6 +17,13 @@ metab.mle2 <- function(do.obs, do.sat, k.gas, z.mix, irr, wtr, ...){
 	return(list("params"=pars, "metab"=c("GPP"=GPP,"R"=R)))
 }
 
+# double *alpha, double *doobs, double *c1, double *c2, double *beta, double *irr, double *wtr, double *kz, double *dosat, int *nobs
+mleLoopR <- function(alpha, doobs, c1, c2, beta, irr, wtr, kz, dosat){
+	nobs <- length(doobs)
+	a.loop <- .C("mleLoopC", alpha=as.double(alpha), as.double(doobs), as.double(c1), as.double(c2), as.double(beta), as.double(irr), as.double(wtr), as.double(kz), as.double(dosat), as.integer(nobs), PACKAGE="LakeMetabolizer")
+	# a.loop[["alpha"]]
+	return(a.loop[["alpha"]])
+}
 
 mle2NLL <- function(Params, do.obs, do.sat, k.gas, z.mix, irr, wtr){
 	c1 <- Params[1] #PAR coeff
