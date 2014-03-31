2ca98440b27d4686de955ae7839dd6694d769cfb
rbatt <battrd@gmail.com>
2014-03-31 15:49:15 -0500

    added notes on other ways to do nep in metab.ols

--- R/metab.ols.R
+++ R/metab.ols.R
@@ -32,7 +32,9 @@ metab.ols <- function(do.obs, do.sat, irr, k.gas, z.mix){
 	gpp <- iota + sum(irr)
 	nep <- sum(fitted(mod))
 	#other ways to get nep:
-	# nep = 
+	# nep = rho + gpp
+	# nep = sum(fitted(mod), na.rm=TRUE) # even if there are NA's in the response variable, they shouldn't be included in fitted() ....
+	# sum(noflux.do.diff) # i think this should be the same as sum(fitted(mod)) b/c model residuals sum to 0 ... right?
 	# also note that NEP is gpp+rho (rho is negative by this convention, which is consistent w/ Kalman, Bayes, mle – unsure of BK)
 
 	# return(list(rho=rho, gpp=gpp, iota=iota))
