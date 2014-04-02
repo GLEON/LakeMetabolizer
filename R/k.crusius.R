fb57449fb33780f0ee7c84db9f04c7d8534362eb
Luke Winslow <lawinslow@gmail.com>
2014-04-02 17:12:32 -0500

    Added comment and some error handling to k.crusius.

--- R/k.crusius.R
+++ R/k.crusius.R
@@ -13,13 +13,18 @@
 k.crusius <- function(wnd,method='power'){
 
   U10 = wnd  #This function uses just the wind speed it is supplied. 
-  
+  method = lower(method)
   if (method=="constant"){
     k600 <- ifelse(U10<3.7,1,5.14*U10-17.9)
   } else if (method=="bilinear") {
     k600 <- ifelse(U10<3.7,0.72*U10,4.33*U10-13.3)
   } else if (method=="power") {
     k600 <- 0.228*U10^2.2+0.168 # units in cm h-1
+  #} else if (method=="linear"){
+  # Note, the linear model equation is not included here as neither the fitted linear model
+  # nor the linear model forced through the origin have slope values in the MS.
+  } else {
+    stop('method must be one of four methods {power, constant, bilinear, and linear')
   }
   
   k600 <- k600*24/100 #units in m d-1
