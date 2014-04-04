2359f236754d1e3275d68b60d030848bf6138826
Luke Winslow <lawinslow@gmail.com>
2014-04-04 11:26:28 -0500

    Added simple metadata files and load.meta function. Terrible idea?

---
+++ R/load.meta.R
@@ -0,0 +1,33 @@
+#load.meta
+
+load.meta = function(fPath){
+  
+  fid = file(fPath, 'rt')
+  tmp = readLines(fid, 100)
+  
+  tmp = strsplit(tmp, '[\t,]') #splits on tabs *and* commas
+  
+  header = tolower(tmp[[1]])
+  
+  id.col = which(header=='id')
+  val.col = which(header=='value')
+  
+  if(length(id.col) == 0 || length(val.col) == 0){
+    stop('Metadata file must contain "ID" and "Value" header elements.')
+  }
+  
+  output = list()
+  
+  for(i in 2:length(tmp)){
+    id = tolower(tmp[[i]][id.col])
+    val = as.numeric(tmp[[i]][val.col])
+    
+    if(is.na(val)){
+      val = tmp[[i]][val.col]
+    }
+    output[id] = val
+  }
+  
+  return(output)  
+  
+}
