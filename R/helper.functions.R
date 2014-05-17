a83b1d326178420027b64cb7ef59d88361d0aab4
Ryan Batt <battrd@gmail.com>
2014-05-16 21:28:28 -0500

    added round to nearest time interval

--- R/helper.functions.R
+++ R/helper.functions.R
@@ -95,3 +95,114 @@ pred.merge <- function(x1, x2, all=FALSE){
 		list(desired=desired.oF, merge=out.aF)
 	}
 }
+
+
+
+# =======================
+# = round.time Function =
+# =======================
+# RDB 16May2014
+# example: round.time(x, "5 minutes")
+# example: round.time(x, "90 min")
+# example: round.time(x, "0.5 hours")
+# x is a time format – preferably POSIXct, or can be coerced w/ as.POSIXct
+# if x needs to be converted to POSIX, define input.format if x currently isn't in a 'standard unambiguous format'
+round.time <- function(x, units, input.format=NULL, output.format="%Y-%m-%d %H:%M:%S"){
+	# x = head(t.sonde0.na2[,"date"], 20) + 120
+	# units = "df.345 min"
+	# Check for invalid input classes
+	stopifnot(is.character(units)&is.character(output.format)&(is.null(input.format)|is.character(input.format)))
+	
+	# Determine time unit
+	unit.choices <- c("sec", "min", "hour", "day")
+	choices.or <- paste(unit.choices, collapse="|")
+	unit.pattern <- paste(".*(", choices.or, ").*", sep="")
+	unit <- gsub(unit.pattern, "\\1", units)
+	if(is.na(unit)){stop("not a valid unit, use sec, min, hour, or day")}
+	which.choice <- which(unit==unit.choices)
+	
+	# Determine time interval
+	u.time.pattern <- "(?:[0-9]+\\.[0-9]+)|(?:[0-9]+\\.)|(?:\\.[0-9]+)|(?:[0-9]+)"
+	u.time.char <- regmatches(units, regexpr(u.time.pattern, units, perl=TRUE))
+	u.time <- as.numeric(u.time.char)
+	u.time <- ifelse(is.na(u.time), 1, u.time)
+	
+	unit.cutoff <- switch(unit, sec=60, min=60, hour=24, day=1)
+	
+	# =========================================================================
+	# = Check for invalid input (before slow [attempted] conversion to POSIX) =
+	# =========================================================================
+	if(sign(u.time)==-1L){
+		stop("time interval must be positive")
+	}
+	# Deal with case where units are 1 second (or less)
+	if(unit=="sec" & u.time<=1L){
+		return(format.Date(x, format=output.format))
+	} else
+	
+	# Fractional time intervals – convert to smaller unit
+	if((trunc(u.time)-u.time)!=0){
+		if(sign(u.time)==1L){
+			while((trunc(u.time)-u.time)!=0){
+				if(unit=="sec"){stop("time interval must be an integer when converted to units of seconds")}
+				unit <- unit.choices[which.choice-1]
+				which.choice <- which(unit==unit.choices)
+				unit.cutoff <- switch(unit, sec=60, min=60, hour=24)
+				u.time <- unit.cutoff*u.time
+			}
+		}else{
+			stop("time interval must be positive")
+		}
+	} else 
+	
+	# Deal with case where units are days
+	if(unit=="day"){
+		if(u.time==1){
+			return(format.Date(trunc.POSIXt(x + 43200, units = units), format=output.format))
+		}else{
+			stop("units must be <= 1 day")
+		}
+	} else 
+	
+	# Deal w/ cases where time interval is 1 unit
+	if(u.time==1){
+			unit <- unit.choices[which.choice-1]
+			which.choice <- which(unit==unit.choices)
+			unit.cutoff <- switch(unit, sec=60, min=60, hour=24)
+			u.time <- unit.cutoff
+	} 
+	
+	# Deal with cases where time interval is > 1 of a larger unit
+	# Note that this follows up on case where u.time is > 1 and is not an integer
+	if(u.time>unit.cutoff){
+		u.time <- u.time%%unit.cutoff
+		mod.mess <- paste("Rounding to units =", u.time, unit) # may or may not want to make this a warning ...
+		warning(mod.mess)
+	}
+	
+	# =============================================
+	# = Convert to POSIX, or if can't, give error =
+	# =============================================
+	if(all(class(x)!="POSIX.ct")){
+		if(is.null(input.format)){
+			x <- as.POSIXct(x)
+		}else{
+			x <- as.POSIXct(x, format=input.format)
+		}
+	}
+	
+	# ===========================================================
+	# = Matching units (e.g., min) and unit multiples (e.g., 5) =
+	# ===========================================================
+	
+	which.choice <- which(unit==unit.choices)
+	form.unit <- c("%S", "%M", "%H", "%d")[which.choice]
+	mult <- as.integer(format.Date(x, format=form.unit))/u.time
+	after <- round(mult, 0)*u.time
+	# direction <- sign(after-before)
+	
+	# trunc.unit <- unit.choices[min(which.choice+1, length(unit.choices))]
+	trunc.unit <- unit.choices[min(which.choice+1, length(unit.choices))]
+	rounded <- trunc.POSIXt(x, trunc.unit) + switch(unit, sec = 1, min = 60, hour = 3600, day = 86400)*after
+	format.Date(rounded, format=output.format)
+}
--- R/o2.saturation.R
+++ R/o2.saturation.R
@@ -18,61 +18,60 @@
 # Initial Author: Luke Winslow
 ###########################################################################
 
-o2.at.sat = function(x, ...) UseMethod("o2.at.sat")
+o2.at.sat <- function(x, ...) UseMethod("o2.at.sat")
 
-o2.at.sat.data.frame = function(temp, baro, altitude=0, salinity=0, model='garcia'){
-  if(ncol(temp) > 2){
-    stop('Temp can only have two columns, "datetime" and temperature')
-  }
-  
-  dosat = o2.at.sat(temp[,2], baro, altitude, salinity, model)
-  
-  return(data.frame(datetime=temp$datetime, dosat=dosat)) 
-  
+o2.at.sat.data.frame <- function(temp, baro, altitude=0, salinity=0, model='garcia'){
+	if(ncol(temp) > 2){
+		stop('Temp can only have two columns, "datetime" and temperature')
+	}
+
+	dosat <- o2.at.sat(temp[,2], baro, altitude, salinity, model)
+
+	return(data.frame(datetime=temp$datetime, dosat=dosat))  
 }
 
 
 o2.at.sat.default <- function(temp, baro, altitude=0, salinity=rep(0,length(temp)), model='garcia'){
   
-  
-  if(!missing(baro)){#Calc using barometric pressure
-    press.corr = (baro * 0.0987 - 0.0112)/100
-  }else{
-    press.corr = (0.0000005 * altitude^2 - 0.0118 * altitude + 99.979)/100
-  }
-  
-  
-  if(tolower(model) == 'garcia'){
-  #Garcia, HE, and LI Gordon. 1992. “Oxygen Solubility in Seawater: Better Fitting Equations.” 
-  #Limnology and Oceanography 37 (6). 
-    
-    Ts = log((298.15 - temp)/(273.15 + temp))
-    
-    lnC = 2.00856 + 3.22400 *Ts + 3.99063*Ts^2 + 4.80299*Ts^2 + 4.80299*Ts^3 + 9.78188e-1*Ts^4 + 
-      1.71069*Ts^5 - salinity*(6.24097e-3 + 6.93498e-3*Ts + 6.90358e-3*Ts^2 + 4.29155e-3*Ts^3) - 3.1168e-7*salinity^2
-    
-    o2.sat = exp(lnC)  * 1.423 #convert from ml/l to mg/l
-    
-  }else if(tolower(model) == 'weiss'){
-    tempk = temp + 273.15
-    
-    lnC = -173.4292 + 249.6339 * (100 / tempk) + 143.3483 *
-          log(tempk / 100) - 21.8492 * (tempk / 100) + 
-          salinity * (-0.033096 + 0.014259 * (tempk / 100) - 0.0017000 * (tempk / 100)^2)
-                        
-    o2.sat = exp(lnC) * 1.423 #convert from ml/l to mg/l
-  
-  }else if(tolower(model) == 'benson'){
-    ## TODO: Fix this to include salinity
-    if(!all(salinity==0)){
-      warning('Benson model does not currently include salinity')
-    }
-    
-    o2.sat = (-0.00006 * (temp)^3) + (0.00725 * (temp)^2) - (0.39571 * (temp)) + 14.59030
-    
-  }
 
-  o2.sat = o2.sat * press.corr
-  
-  return(o2.sat)
+	if(!missing(baro)){#Calc using barometric pressure
+		press.corr <- (baro * 0.0987 - 0.0112)/100
+	}else{
+		press.corr <- (0.0000005 * altitude^2 - 0.0118 * altitude + 99.979)/100
+	}
+
+
+	if(tolower(model) == 'garcia'){
+	#Garcia, HE, and LI Gordon. 1992. “Oxygen Solubility in Seawater: Better Fitting Equations.” 
+	#Limnology and Oceanography 37 (6). 
+
+	Ts <- log((298.15 - temp)/(273.15 + temp))
+
+	lnC <- 2.00856 + 3.22400 *Ts + 3.99063*Ts^2 + 4.80299*Ts^2 + 4.80299*Ts^3 + 9.78188e-1*Ts^4 + 
+	1.71069*Ts^5 - salinity*(6.24097e-3 + 6.93498e-3*Ts + 6.90358e-3*Ts^2 + 4.29155e-3*Ts^3) - 3.1168e-7*salinity^2
+
+	o2.sat <- exp(lnC)  * 1.423 #convert from ml/l to mg/l
+
+	}else if(tolower(model) == 'weiss'){
+		tempk <- temp + 273.15
+
+		lnC <- -173.4292 + 249.6339 * (100 / tempk) + 143.3483 *
+		log(tempk / 100) - 21.8492 * (tempk / 100) + 
+		salinity * (-0.033096 + 0.014259 * (tempk / 100) - 0.0017000 * (tempk / 100)^2)
+          
+		o2.sat <- exp(lnC) * 1.423 #convert from ml/l to mg/l
+
+	}else if(tolower(model) == 'benson'){
+		## TODO: Fix this to include salinity
+		if(!all(salinity==0)){
+			warning('Benson model does not currently include salinity')
+		}
+
+	o2.sat <- (-0.00006 * (temp)^3) + (0.00725 * (temp)^2) - (0.39571 * (temp)) + 14.59030
+
+	}
+
+	o2.sat <- o2.sat * press.corr
+
+	return(o2.sat)
 }
