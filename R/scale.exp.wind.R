1b995c5ddb69aa43bc407f3b825f9d533bb0a68b
Luke Winslow <lwinslow@usgs.gov>
2014-06-01 12:55:49 -0700

    Added timeseries form of scale.exp.wind.

--- R/scale.exp.wind.R
+++ R/scale.exp.wind.R
@@ -1,7 +1,26 @@
 
+scale.exp.wind <- function(ts.data, wndZ){
+	wnd = get.vars(tb.data$data, 'wnd')
+	
+	if(missing(wndZ)){
+		wndZ = get.offsets(wnd)
+    if(is.na(wndZ)){
+			stop('Unknown wind height. Must supply wndZ parameter or have offset defined in header of ts.data.')
+		}
+	}
+	
+	if(ncol > 2 || length(wndZ) > 1){
+		stop('too many columns supplied to scale.exp.wind. Please supply only one datetime and wnd columns.')
+	}
+	
+	u10 = scale.exp.wind.base(wnd[,2], wndZ)
+	
+	return(data.table(datetime=ts.data$datetime, wnd_10=u10))
+}
+
 #Reference for this
 #Arya 1988 (Introduction to micrometeorology)
-scale.exp.wind <- function(wnd, wndHeight){
-	U10 <- wnd * (10/wndHeight)^(1/7) # should this be ^(1.7)?? I've used ^0.15 ... Changing for now. -RDB
+scale.exp.wind.base <- function(wnd, wndZ){
+	U10 <- wnd * (10/wndHeight)^(1/7) 
 	return(U10)
 }
