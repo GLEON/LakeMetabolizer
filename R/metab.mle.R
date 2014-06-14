1009f129a44b8f051c7e4d113ce40146d2a9f4df
rbatt <battrd@gmail.com>
2014-06-13 19:23:21 -0500

    changed mle2NLL to mleNLL (was relic of when there were R & C versions; deleted comments

--- R/metab.mle.R
+++ R/metab.mle.R
@@ -1,5 +1,7 @@
 
-
+# ====================
+# = Main mle wrapper =
+# ====================
 metab.mle <- function(do.obs, do.sat, k.gas, z.mix, irr, wtr, ...){
 
 	nobs <- length(do.obs)
@@ -31,7 +33,7 @@ metab.mle <- function(do.obs, do.sat, k.gas, z.mix, irr, wtr, ...){
 	Q0 <- ((diff(range(do.obs,na.rm=TRUE)) - mean(do.obs,na.rm=TRUE))^2 / length(do.obs))
 	guesses <- c(1E-4, 1E-4, log(Q0))
 	
-	fit <- optim(guesses, fn=mle2NLL, do.obs=do.obs, do.sat=do.sat, k.gas=(k.gas/freq), z.mix=z.mix, irr=irr, wtr=wtr, ...)
+	fit <- optim(guesses, fn=mleNLL, do.obs=do.obs, do.sat=do.sat, k.gas=(k.gas/freq), z.mix=z.mix, irr=irr, wtr=wtr, ...)
 	pars0 <- fit$par
 	
 	pars <- c("gppCoeff"=pars0[1], "rCoeff"=pars0[2], "Q"=exp(pars0[3]))
@@ -40,21 +42,24 @@ metab.mle <- function(do.obs, do.sat, k.gas, z.mix, irr, wtr, ...){
 	# = Use fits to calculate metabolism =
 	# ====================================
 	GPP <- mean(pars[1]*irr, na.rm=TRUE) * freq
-	# GPP <- mean(pars[1]*irr, na.rm=TRUE)*Freq # Use a line like this if we are going to have missing values; (	Freq <- round(Mode(1/diff(do.doy)))  from metabData, line 21)
 	R <- mean(pars[2]*log(wtr), na.rm=TRUE) * freq
 	
 	return(list("params"=pars, "metab"=c("GPP"=GPP,"R"=R,"NEP"=GPP+R)))
 }
 
-# double *alpha, double *doobs, double *c1, double *c2, double *beta, double *irr, double *wtr, double *kz, double *dosat, int *nobs
+# ==========================
+# = The R loop for mle NLL =
+# ==========================
 mleLoopR <- function(alpha, doobs, c1, c2, beta, irr, wtr, kz, dosat){
 	nobs <- length(doobs)
 	a.loop <- .C("mleLoopC", alpha=as.double(alpha), as.double(doobs), as.double(c1), as.double(c2), as.double(beta), as.double(irr), as.double(wtr), as.double(kz), as.double(dosat), as.integer(nobs), PACKAGE="LakeMetabolizer")
-	# a.loop[["alpha"]]
 	return(a.loop[["alpha"]])
 }
 
-mle2NLL <- function(Params, do.obs, do.sat, k.gas, z.mix, irr, wtr){
+# ====================
+# = mle NLL function =
+# ====================
+mleNLL <- function(Params, do.obs, do.sat, k.gas, z.mix, irr, wtr){
 	c1 <- Params[1] #PAR coeff
 	c2 <- Params[2] #log(Temp) coeff
 	Q <- exp(Params[3]) # Variance of the process error
