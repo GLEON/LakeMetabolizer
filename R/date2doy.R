5614808b90ef7026c34d8873a5841f92a39bdcc6
Ryan Batt <battrd@gmail.com>
2014-06-15 13:58:15 -0500

    fixed bug in date2doy where midnight was specified under local timezone, not tz=GMT

--- R/date2doy.R
+++ R/date2doy.R
@@ -2,7 +2,7 @@ date2doy <- function(x){
 	stopifnot(any(grepl("^POSIX",class(x[1]))))
 	day <- as.numeric(format(x, "%j"))
 	pat <- quote("([0-9]{2}:){2}[0-9]{2}")
-	midnight <- gsub(pat, "00:00:00", x)
+	midnight <- as.POSIXct(gsub(pat, "00:00:00", x), tz="GMT")
 	frac <- as.numeric(difftime(x, midnight, units="days"))
 	day+frac
 }
