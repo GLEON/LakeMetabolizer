99896dd403920bf76f9494f8604f67cb8f541009
Luke Winslow <lwinslow@usgs.gov>
2015-04-15 15:38:37 -0500

    Some minor changes

--- demo/fig_k600.R
+++ demo/fig_k600.R
@@ -13,7 +13,7 @@ u10 = wind.scale(ts.data)
 ts.data = rmv.vars(ts.data, 'wnd', ignore.offset=TRUE) #drop old wind speed column
 ts.data = merge(ts.data, u10)                          #merge new u10 into big dataset
 
-
+#Calculate k600 using the k.cole wind-based model
 k600_cole = k.cole(ts.data)
 
 k600_crusius = k.crusius(ts.data)
@@ -26,7 +26,6 @@ lake.area = tb.data$metadata$lakearea
 
 #for k.read and k.macIntyre, we need LW_net.
 #Calculate from the observations we have available.
-
 lwnet = calc.lw.net(ts.data, lat, atm.press)
 ts.data = merge(ts.data, lwnet)
 ## Not run: 
@@ -38,6 +37,8 @@ k600_soloviev = k.read.soloviev(ts.data, wnd.z=wnd.z, Kd=kd,
 
 k600_macIntyre = k.macIntyre(ts.data, wnd.z=wnd.z, Kd=kd, atm.press=atm.press)
 
+
+#Create plot and save in user home directory (on Windows, Documents folder, on Mac, Home folder)
 png('~/k600_figure.png', res=300, width=3000, height=1500)
 cols = rainbow(5)
 plot(k600_macIntyre, type='l', xlab='', col=cols[1], ylim=c(0,10), ylab=expression(k600~(m^-1)))
