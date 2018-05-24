db5e0bcad565b02d8fc8371139d53a458ee56c6d
Luke Winslow <lawinslow@gmail.com>
2018-05-24 12:09:41 -0400

    addressing issue #129

--- R/k.read.R
+++ R/k.read.R
@@ -370,7 +370,7 @@ k.read.base <- function(wnd.z, Kd, lat, lake.area, atm.press, dateTime, Ts, z.am
     visTable <- c(1.792,1.519,1.308,1.141,1.007,0.897,
                   0.804,0.727,0.661,0.605,0.556,0.513,0.477,0.444,
                   0.415,0.39,0.367,0.347,0.328,0.311,0.296)
-    v <- data.frame(approx(tempTable,visTable,xout = Ts))[2]
+    v <- approx(tempTable,visTable,xout = Ts)$y
     v <- v*1e-6
     return(v)
   }
